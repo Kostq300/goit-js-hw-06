885ee6de8b634f6212a783780d43d1372eecd86c
Oleynik Konstantin <oleynik.kostia2@gmail.com>
2022-08-28 00:48:40 +0300

    2 task complete

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,22 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulEl = document.querySelector('ul');
+
+  let ingredientsList = [];
+
+  for (const ingredient of ingredients) {
+    
+    const liEl = document.createElement('li');
+    liEl.textContent = ingredient;
+    liEl.classList.add('item');
+
+    ingredientsList.push(liEl);
+
+  }
+
+ulEl.append(...ingredientsList);
+
+
+ 
